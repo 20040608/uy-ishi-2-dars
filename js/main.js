313ca3e2392edaede342e2c7c8e25041526a884c
20040608 <issakvell@gmail.com>
2022-09-13 21:48:39 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,435 @@
+//----------------- 1-masala
+
+// var a = prompt('Son kiriting !')
+
+// var a = 18
+
+// if (a >= 0){
+//     console.log('Bu butun son');
+// }else {
+//     console.log('Bu butun son emas');
+// }
+
+
+
+// ----------------- 2-masala
+
+// var a = prompt('Son kiriting !')
+
+// var a = 17
+
+// if (a % 2 !== 0){
+//     console.log('toq son');
+// } else {
+//     console.log('juft son');
+// }
+
+
+
+// ----------------- 3-masala
+
+// var a = prompt('Son kiriting !')
+
+// var a = 13
+
+// if (a % 2 === 0){
+//     console.log('juft son');
+// } else {
+//     console.log('toq son');
+// }
+
+
+// ----------------- 4-masala
+
+// var a = prompt('Son kiriting !')
+// var b = prompt('Son kiriting !')
+
+// var a = -14
+// var b = -15
+
+// if (a > 0 && b > 0 ) {
+//     console.log('Bu sonlar 0 dan katta !');
+// }else if (a < 0 && b > 0) {
+//     console.log('a ga berilgan son 0 dan kichkina !');
+// } else if (a > 0 && b < 0) {
+//     console.log('b ga berilgan son 0 dan kichkina !');
+// } else {
+//     console.log('Bu sonlar 0 dan kichkina !');
+// }
+
+
+
+// ----------------- 5-masala
+
+// var a = 17
+// var b = 15
+// var c = 13
+
+// if ( a > b && b > c ) {
+//     console.log('a b dan katta va b c dan katta');
+// } 
+
+
+
+// ----------------- 6-masala
+
+// var a = 0
+// var b = 11
+// var c = 10
+
+// if ( a >= b || b <= c ) {
+//     console.log('a va c ortasida yotibdi');
+// } else {
+//     console.log('a va c ortasida yoq');
+// }
+
+
+
+// ----------------- 7-masala
+
+// var a = 8
+// var b = 6
+
+// if (a % 2 === 0 && b % 2 === 0){
+//     console.log('bular juft sonlar');
+// } else if (a % 2 !== 0 && b % 2 === 0){
+//     console.log('a da toq son');
+// } else if (a % 2 === 0 && b % 2 !== 0){
+//     console.log('b da toq son');
+// }else {
+//     console.log('bular toq sonlar');
+// }
+
+
+
+// ----------------- 8-masala
+
+// var a = -20
+// var b = -25
+// var c = -1
+// var d = -8
+
+// if (a < 0 && b < 0 && c < 0 && d < 0) {
+//     console.log('a b c d sonlari 0 dan kichkina');
+// } else if (a > 0 && b > 0 && c > 0 && d > 0) {
+//     console.log('hamma sonlar 0 dan katta');
+// }else if (a > 0 && b >0 &&  c > 0) {
+//     console.log('a, b va c 0 dan katta');
+// } else if (a > 0 && b >0 &&  d > 0) {
+//     console.log('a, b va d 0 dan katta');
+// } else if (d > 0 && b >0 &&  c > 0) {
+//     console.log('d, b va c 0 dan katta');
+// } else if (a > 0 && d >0 &&  c > 0) {
+//     console.log('a, b va c 0 dan katta');
+// } else if (d > 0 && a >0 ) {
+//     console.log('d va a 0 dan katta');
+// } else if (d > 0 && b >0 ) {
+//     console.log('d va b 0 dan katta');
+// } else if (d > 0 && c >0 ) {
+//     console.log('d va c 0 dan katta');
+// } else if (a > 0 && b >0 ) {
+//     console.log('a va b 0 dan katta');
+// } else if (c > 0 && a >0 ) {
+//     console.log('c va a 0 dan katta');
+// } else if (b > 0 && c >0 ) {
+//     console.log('c va b 0 dan katta');
+// } else if (a > 0) {
+//     console.log('a 0 dan katta');
+// } else if (b > 0) {
+//     console.log('b 0 dan katta');
+// } else if (c > 0) {
+//     console.log('c 0 dan katta');
+// } else if (d > 0) {
+//     console.log('d 0 dan katta');
+// }                  
+
+
+
+// ----------------- 9-masala
+
+// var a = -10
+// var b = -48
+// var c = -40
+
+// if (a < 0 && b < 0 && c < 0){
+//     console.log('Hammasi 0 dan kichkina');
+// } else if (a > 0 && b > 0){
+//     console.log('a va b 0 dan katta');
+// } else if (a > 0 && c > 0){
+//     console.log('a va c 0 dan katta');
+// } else if (c > 0 && b > 0){
+//     console.log('c va b 0 dan katta');
+// } else if (a > 0){
+//     console.log('a 0 dan katta');
+// } else if (b > 0){
+//     console.log('b 0 dan katta');
+// } else if (c > 0){
+//     console.log('c 0 dan katta');
+// }
+
+
+
+// ----------------- 10-masala
+
+// var a = prompt('Hafta kunlarini raqamini kiriting 1 dan 7 gacha')
+
+// if (a == 1){
+//     console.log('Dushanba');
+// } else if (a == 2){
+//     console.log('Seshanba');
+// } else if (a == 3){
+//     console.log('Chorshanba');
+// } else if (a == 4){
+//     console.log('Payshanba');
+// } else if (a == 5){
+//     console.log('Juma');
+// } else if (a == 6){
+//     console.log('Shanba');
+// } else if (a == 7){
+//     console.log('Yakshanba');
+// } else if ( a <= 8 && a >= 0 ){
+//     console.log('Hafta kunlarini raqamlarini kiriting iltimos');
+// } 
+
+
+
+// ----------------- 11-masala
+
+
+
+
+//  Uzur ustoz bu savolga tushunmadim
+
+
+
+
+
+// ----------------- 12-masala
+
+// var a = prompt('son kiriting')
+// var a = -18
+
+// if (a > 0){
+//     console.log(a + 1);
+// }else {
+//     console.log(a - 1);
+// }
+
+
+
+// ----------------- 13-masala
+
+// var a = prompt('son kiriting')
+// var a = -18
+
+// if (a > 0){
+//     console.log(a * 3);
+// }else {
+//     console.log(a - 2);
+// }
+
+
+
+// ----------------- 14-masala
+
+// var a = 12
+// var b = 12
+
+// if (a > b){
+//     console.log('a dagi soni katta');
+// } else if (a < b){
+//     console.log('b dagi soni katta');
+// } else {
+//     console.log('Bir-biriga teng!');
+// }
+
+
+
+// ----------------- 15-masala
+
+// var a = 6
+// var b = 1
+// var c = 4
+
+// if (a > b && b > c){
+//     console.log('eng kattasi a sonida');
+// } else if (b > a && a > c){
+//     console.log('eng kattasi b sonida');
+// } else if (c > a  && a > b){
+//     console.log('eng kattasi c sonida');
+// } else if (a > b && b < c){
+//     console.log('eng kattasi a sonida');
+// } else if (b > a && a < c){
+//     console.log('eng kattasi b sonida');
+// } else if (c > a  && a < b){
+//     console.log('eng kattasi c sonida');
+// }
+
+
+
+// ----------------- 16-masala
+
+// var a = 2
+// var b = 7
+// var c = 4
+
+// if (a < b && b < c){
+//     console.log('eng kichigi a sonida');
+// } else if (b < a && a < c){
+//     console.log('eng kichigi b sonida');
+// } else if (c < a  && c < b){
+//     console.log('eng kichigi c sonida');
+// } else if (a < b && b > c){
+//     console.log('eng kichigi a sonida');
+// } else if (b < a && a > c){
+//     console.log('eng kichigi b sonida');
+// } else if (c < a  && a > b){
+//     console.log('eng kichigi c sonida');
+// } 
+
+
+
+// ----------------- 17-masala
+
+
+// var a = prompt('Passport yoki Metirkadagi jinsizni kiriting')
+
+// if (a == 'erkak'){
+//     console.log('Erkak');
+// } else if (a == 'ayol') {
+//     console.log('Ayol');
+// }
+
+
+
+// ----------------- 18-masala
+
+// var a = prompt('Olayotgan baholarizni birini kiriting ')
+
+// if (a == 5) {
+//     console.log('Siz oy ohirida 100 ming qoshimcha pul olasiz');
+// } else if (a == 4) {
+//     console.log('Siz oy ohirida 50 ming qoshimcha pul olasiz');
+// } else if (a == 3) {
+//     console.log('Siz oy ohirida qoshimcha pul olmiysiz');
+// } else if (a == 2) {
+//     console.log('Siz oqishga harakat qiling bolmasa yiqilib ketasiz');
+// } else {
+//     console.log('Hazilsiz bahoyizni kiriting iltimos');
+// } 
+
+
+
+// ----------------- 19-masala
+
+// var a = 1
+
+// if(a > 0 && a <= 9 ){
+//     console.log('toq musbat son');
+// } else if ( a < 0 && a >= -9) {
+//     console.log('toq manfiy son');
+// } else if (a >= 10){
+//     console.log('juft musbat son');
+// } else if ( a <= -10 ){
+//     console.log('juft manfiy son');
+// }
+
+
+
+// ----------------- 20-masala
+
+// var a = prompt('yilingizni kiriting')
+
+// if (2022 - a ){
+//     console.log(2022 - a );
+// }
+
+
+
+// ----------------- 21-masala
+
+// var a = 14
+// var b = 14
+// var c = 14
+
+// if (a <= b <= c){
+//     console.log('bir xil raqam bolsa togri');
+// }else {
+//     console.log('bir xil raqam emas');
+// }
+
+
+
+// ----------------- 22-masala
+
+// var a = 5
+// var b = 1
+
+// if(a % 2 === 0 && b % 2 === 0){
+//     console.log('bular juft sonlar');
+// } else if (a % 2 !== 0 && b % 2 === 0){
+//     console.log('a toq son');
+// } else if (a % 2 === 0 && b % 2 !== 0){
+//     console.log('b toq son');
+// } else {
+//     console.log('bular toq sonlar');
+// }
+
+
+
+// ----------------- 23-masala
+
+// var a = 99
+// var b = 15
+// var c = 18
+
+
+// if (a > 0 && b > 0 && c > 0) {
+//     console.log('hamma sonlar 0 dan katta');
+// } else if (a < 0 && b < 0 && c < 0) {
+//     console.log('a b c sonlari 0 dan kichkina');
+// } else if (a > 0 && b >0 ) {
+//     console.log('a va b  0 dan katta');
+// } else if (b >0 && c > 0) {
+//     console.log('b va c 0 dan katta');
+// } else if (a > 0 &&  c > 0) {
+//     console.log('a va c 0 dan katta');
+// } else if (a > 0) {
+//     console.log('a 0 dan katta');
+// } else if (b > 0) {
+//     console.log('b 0 dan katta');
+// } else if (c > 0) {
+//     console.log('c 0 dan katta');
+// }        
+
+
+
+// ----------------- 24-masala
+
+// var a = 20
+// var b = -25
+// var c = 1
+
+
+// if (a > 0 && b > 0 && c > 0) {
+//     console.log('hamma sonlar 0 dan katta');
+// } else if (a < 0 && b < 0 && c < 0) {
+//     console.log('a b c sonlari 0 dan kichkina');
+// } else if (a > 0 && b >0 ) {
+//     console.log('a va b  0 dan katta');
+// } else if (b >0 && c > 0) {
+//     console.log('b va c 0 dan katta');
+// } else if (a > 0 &&  c > 0) {
+//     console.log('a va c 0 dan katta');
+// } else if (a > 0) {
+//     console.log('a 0 dan katta');
+// } else if (b > 0) {
+//     console.log('b 0 dan katta');
+// } else if (c > 0) {
+//     console.log('c 0 dan katta');
+// }  
+
+
+// ----------------- 25-masala
+
+//  Uzur ustoz bu savolga tushunmadim
